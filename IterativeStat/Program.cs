e85f2d1959a3017db4d7bd1a8bced4822e548e62
Mariell <Mariell@DESKTOP-V0EECRJ>
2019-01-22 16:22:55 -0500

    Add project files.

---
+++ IterativeStat/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IterativeStat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
